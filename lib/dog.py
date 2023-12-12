a982a411401123339618bc68551aea2a76805427
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-12-12 07:15:57 -0800

    lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -8,8 +8,25 @@ APPROVED_BREEDS = [
     "Beagle",
     "French Bulldog",
     "Pug",
-    "Pointer"
+    "Pointer",
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Mastiff"):
+        self.name = self.validate_name(name)
+        self.breed = self.validate_breed(breed)
+
+    def validate_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            return name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            return "Invalid Name"
+
+    def validate_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            return breed
+        else:
+            print("Breed must be in list of approved breeds.")
+            return "Invalid Breed"
--- lib/person.py
+++ lib/person.py
@@ -12,8 +12,25 @@ APPROVED_JOBS = [
     "General Management",
     "Research & Development",
     "Marketing",
-    "Purchasing"
+    "Purchasing",
 ]
 
+
 class Person:
-    pass
+    def __init__(self, name="Sam", job="Admin"):
+        self.name = self.validate_name(name)
+        self.job = self.validate_job(job)
+
+    def validate_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            return name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            return "Invalid Name"
+
+    def validate_job(self, job):
+        if job in APPROVED_JOBS:
+            return job
+        else:
+            print("Job must be in list of approved jobs.")
+            return "Invalid Job"
